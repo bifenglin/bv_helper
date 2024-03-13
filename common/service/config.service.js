bfbb7f75b5fccf27b515df7c419512935dc7274f
justanyone2023 <youremail@example.com>
2024-03-13 18:26:38 +0800

    change readme
    Signed-off-by: justanyone2023 <youremail@example.com>

--- common/service/config.service.js
+++ common/service/config.service.js
@@ -2,9 +2,9 @@ let BASE_URL = ''
 
 
 if (process.env.NODE_ENV == 'development') {
-    BASE_URL = 'https://bvhp-server-37674f03-cd6a-47a1-aece-51f000c331d8.dev-hz.cloudbaseapp-sandbox.cn/jeecg-boot' // 开发环境
+    BASE_URL = 'http://192.168.1.109' // 开发环境
 } else {
-	BASE_URL = 'https://bvhp-server-37674f03-cd6a-47a1-aece-51f000c331d8.dev-hz.cloudbaseapp-sandbox.cn/jeecg-boot' // 生产环境
+	BASE_URL = 'http://192.168.1.109' // 生产环境
 }
 let staticDomainURL = BASE_URL+ '/sys/common/static';
 
